2c9e3b7a6a1d30f8ff8ffe9e00777ce9130b9406
ahmedd0 <aelkholy177@gmail.com>
2021-04-04 22:14:23 +0200

    delete operation , local storage

--- js/main.js
+++ js/main.js
@@ -6,13 +6,25 @@ var tBody = document.getElementById("tbody");
 var productDesc = document.getElementById("productDesc");
 var addBtn = document.getElementById("add");
 // ======= GLOBAL VARIABLE  ======
-var stock = [];
+var stock, lastId;
+if (localStorage.getItem("lastId") == null) {
+  lastId = 0;
+} else {
+  lastId = localStorage.getItem("lastId");
+}
+
+if (localStorage.getItem("products") == null) {
+  stock = [];
+} else {
+  stock = JSON.parse(localStorage.getItem("products"));
+}
+console.log(stock);
 // ======== CRUD OPERATIONS ================
 var CRUD = {
   //======= ADD PRODUCT TO ARRAY LIST ======
   addProduct: function () {
     var product = {
-      id: stock.length + 1,
+      id: ++lastId,
       name: productName.value,
       cate: productCategory.value,
       price: productPrice.value,
@@ -25,32 +37,39 @@ var CRUD = {
       product.desc != ""
     ) {
       stock.push(product);
+      localStorage.setItem("lastId", lastId.toString());
       return false;
     } else {
       return true;
     }
   },
+
   // ======== RETRIEVE ALL PRODUCT IN TABLE =========
   displayProducts: function () {
     var container = "";
     for (let i = 0; i < stock.length; i++) {
-      container +=
-        "<tr><td>" +
-        stock[i].id +
-        "</td><td>" +
-        stock[i].name +
-        "</td><td>" +
-        stock[i].cate +
-        "</td><td>" +
-        stock[i].price +
-        "</td><td>" +
-        stock[i].desc +
-        "</td> <td class='text-center'><i class='fas fa-edit text-success fa-1x mr-3 edit'></i><i class='fas fa-trash text-danger remove'></i></td></tr>";
+      container += `<tr><td>
+        ${stock[i].id}
+        </td>
+          <td>${stock[i].name}</td>
+          <td>${stock[i].cate}</td>
+          <td>${stock[i].price}</td>
+          <td>${stock[i].desc}</td>
+          <td class='text-center'>
+            <i class='fas fa-edit text-success fa-1x mr-3 edit'></i>
+            <i onclick='CRUD.deleteProduct(${i})' class='fas fa-trash text-danger remove'></i>
+          </td>
+        </tr>`;
     }
     tBody.innerHTML = container;
   },
+  deleteProduct: function (index) {
+    stock.splice(index, 1);
+    localStorage.setItem("products", JSON.stringify(stock));
+    CRUD.displayProducts();
+  },
 };
-
+CRUD.displayProducts();
 // =========== CLEAR INPUTS ======
 function clearInputs() {
   productName.value = "";
@@ -62,8 +81,9 @@ addBtn.onclick = function () {
   var isEmpty = CRUD.addProduct();
   if (!isEmpty) {
     CRUD.displayProducts();
-  }else{
-    alert("PLEASE ENTER ALL PRODUCT INFORMATION")
+  } else {
+    alert("PLEASE ENTER ALL PRODUCT INFORMATION");
   }
   clearInputs();
+  localStorage.setItem("products", JSON.stringify(stock));
 };
